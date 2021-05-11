92856f86c75a378a0264843c124ff35540e2a5aa
Zoltan Haindrich <kirk@rxd.hu>
2021-05-11 19:06:33 +0200

    fix

--- job-dsl/precommit.groovy
+++ job-dsl/precommit.groovy
@@ -1,5 +1,5 @@
 
-['ocp','ocp-7.1','cdpd-master','FENG','dev-compaction-observability-metrics-2021','dev-iceberg-ga'].each { hiveBranch ->
+['ocp','dev-ocp-7.1','cdpd-master','FENG','dev-compaction-observability-metrics-2021','dev-iceberg-ga'].each { hiveBranch ->
   pipelineJob("tx-internal-hive-precommit-${hiveBranch}") {
     logRotator(33, -1, -1, -1)
     properties {
