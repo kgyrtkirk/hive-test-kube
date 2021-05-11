7571ce2882d4335e6abf00954e8aafb4ef038b4e
Zoltan Haindrich <kirk@rxd.hu>
2021-05-11 11:57:02 +0200

    add seed

---
+++ job-dsl/seed.groovy
@@ -0,0 +1,27 @@
+// If you want, you can define your seed job in the DSL and create it via the REST API.
+// See https://github.com/sheehan/job-dsl-gradle-example#rest-api-runner
+
+job('htk-seed') {
+    scm {
+        git {
+            remote {
+                name('kgyrtkirk')
+                url("https://github.com/kgyrtkirk/hive-test-kube")
+        }
+        branch('ocp')
+        extensions {
+          cleanBeforeCheckout()
+          relativeTargetDirectory('hive-test-kube')
+        }
+      }
+    }
+    triggers {
+        scm '*/1 * * * *'
+    }
+    steps {
+        dsl {
+            external 'hive-test-kube/job-dsl/**/*.groovy'
+            removeAction('DELETE')
+        }
+    }
+}
