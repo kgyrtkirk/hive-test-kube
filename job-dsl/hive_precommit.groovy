eafe8c6082d33caf52d7eee201aa8b112267c3b3
Karen Coppage <karen.coppage@cloudera.com>
2021-09-09 16:29:30 +0200

    compaction observability -> cdw

--- job-dsl/hive_precommit.groovy
+++ job-dsl/hive_precommit.groovy
@@ -8,9 +8,9 @@
     'R24':'cdw-master',
     'R25':'cdw-master',
     'cdw-master':'cdw-master',
-    'dev-compaction-observability-metrics-2021':'cdpd-master',
+    'dev-compaction-observability-metrics-2021':'cdw-master',
     'dev-iceberg-ga2':'cdw-master',
-    'dev-compaction-observability-metrics-2021-v2':'cdpd-master',
+    'dev-compaction-observability-metrics-2021-v2':'cdw-master',
     'dev-iceberg-ga':'cdpd-master'].each { hiveBranch,baseBranch ->
   pipelineJob("tx-internal-hive-precommit-${hiveBranch}") {
     logRotator(33, -1, -1, -1)
